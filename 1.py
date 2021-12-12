c14d1f6b3356f7e7a147e0ffc4d6bb53d7371615
Dhanashrikandale123 <92363620+Dhanashrikandale123@users.noreply.github.com>
2021-12-12 12:07:50 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 11 12:30:06 2021
+
+@author: mali-
+"""
+
+#Goal of this tryout is to create a function from scratch and invoke it for the given problem
+def calculate(fara):
+    cel=(fara-32)*(5/9)
+    return cel
+cel=(99-32)*(5/9)
+print(cel)
+def calculate(cel):
+    Fara = (cel * 9/5) + 32
+    return fara
+fara=(37*9/5)+32
+print(fara)
